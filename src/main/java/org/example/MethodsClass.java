d9defd75ca0626eb8eee80feb5109a9125271149
ShaiLevi23 <shailevi23@gmail.com>
2022-11-13 17:49:49 +0200

    methods skeleton

---
+++ src/main/java/org/example/MethodsClass.java
@@ -0,0 +1,70 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MethodsClass<T>{
+
+    //READ
+
+    //get all the items in a table
+//    public List<T> allItemsInTable(){
+//        return;
+//    }
+
+    //Get one item by id
+//    public <T> T getItemById(int id){
+//        return;
+//    }
+
+    //Get one/many item by any property (get user(s) by name)
+//    public <T> T getItemByName(String property){
+//        return;
+//    }
+
+
+
+
+    //ADD
+
+    //Add a single item to a table
+//    public void addSingleItem(Class<T> item){
+//
+//    }
+
+    //Add multiple items
+//    public void addMultipleItems(List<T> item){
+//
+//    }
+
+
+
+
+
+    //UPDATE
+
+    //Update a single property of a single item (update email for user with id x)
+//    public void updateSinglePropertyOfSingleItem(Class<T> property, Class<T> item){
+//
+//    }
+
+
+}
+
+
+//Update a single property of a single item (update email for user with id x)
+//Update an entire item
+//Delete
+//Single item deletion by any property (delete user with email x)
+//Multiple item deletion by any property (delete all users called x)
+//Delete entire table (truncate)
+//Table creation
+//Create a table based on an entity
+//Error handling
+//Return relevant errors to the client (no SQLExceptions)
+//Extra features
+//Auto incrementation on fields
+//Mark fields as primary key
+//Mark fields as unique
+//Fields that are not of primitive types should be saved in the db as json string.
+//** Settings of specific fields should be done with annotations.
