462fa4303ba700fc5194b8df1a22cc20ea71abd6
Omar Hmdea <omarhmdea@MacBook-Pro-Omar.local>
2022-11-14 15:43:13 +0200

    dd

--- src/main/java/org/example/ORM/Repository.java
+++ src/main/java/org/example/ORM/Repository.java
@@ -1,64 +1,68 @@
 package org.example.ORM;
 
 import java.lang.reflect.Field;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Repository<T> {
+        private  Class<T> clz;
 
-    private final Class<T> clz;
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/";
-    private Connection connection;
+        public Repository(Class<T> clz) {
+            this.clz = clz;
+        }
 
-
-    public Repository(Class<T> clz, Connection connection) {
-        this.clz = clz;
-        this.connection = connection;
-    }
-
-    public void createTable() {
-        StringBuilder stringBuilder = createTableQuery();
-        execute(stringBuilder.toString());
-    }
-
-
-    public StringBuilder createTableQuery() {
+    public StringBuilder createTableByEntity(Class<T> entity){
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("CREATE TABLE ");
-        stringBuilder.append(clz.getSimpleName().toLowerCase());
+        stringBuilder.append(entity.getSimpleName().toLowerCase());
         stringBuilder.append(" (\n");
 
-        for(Field field : clz.getDeclaredFields()) {
+        for (Field field : entity.getDeclaredFields()) {
             stringBuilder.append(field.getName());
             stringBuilder.append(" ");
-            stringBuilder.append(getMySQLDataType(field.getType().getSimpleName()));
+            if(field.getType().getSimpleName().equals("int")) {
+                stringBuilder.append("int(11)");
+            }
+            if(field.getType().getSimpleName().equals("String")){
+                stringBuilder.append("varchar(255)");
+            }
+            if(field.getType().getSimpleName().equals("Double")){
+                stringBuilder.append("double(5,3)");
+            }
+            if(field.getType().getSimpleName().equals("List")){
+                stringBuilder.append("varchar(255)");
+            }
             stringBuilder.append(",\n");
         }
-        stringBuilder.replace(stringBuilder.toString().length() - 2, stringBuilder.toString().length(), "\n);");
+        stringBuilder.replace(stringBuilder.toString().length() -2, stringBuilder.toString().length(), "\n);");
+        System.out.println(stringBuilder);
         return stringBuilder;
     }
 
-
-    public void connect(String user, String password) {
+    public Connection connect(){
+        Connection connection = null;
         try {
+            // below two lines are used for connectivity.
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection =  DriverManager.getConnection(DB_URL + "summery_project", user, password);
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/summery_project",
+                    "root", "omar135790864");
 
-        } catch(ClassNotFoundException | SQLException e) {
-            throw new RuntimeException(e);
         }
+        catch (Exception exception) {
+            System.out.println(exception);
+        }
+        return connection;
+    } // function ends
+
+    public void execute(String quary, Connection connection) throws SQLException {
+        Statement statement;
+        statement = connection.createStatement();
+        statement.execute(quary);
 
     }
-
-    public void execute(String query) {
-        try{
-            Statement statement = this.connection.createStatement();
-            statement.execute(query);
-
-        } catch(SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 
     private String createFindQuery(String field) {
         StringBuilder sb = new StringBuilder();
@@ -68,30 +72,5 @@ public class Repository<T> {
         sb.append(clz.getSimpleName());
         sb.append(" WHERE " + field + "=?");
         return sb.toString();
-    }
-
-
-    public String getMySQLDataType(String javaType) {
-        switch(javaType) {
-            case "int":
-            case "Integer":
-                return "int(11)";
-            case "long":
-            case "Long":
-                return "BIGINT(50)";
-            case "float":
-            case "Float":
-                return "FLOAT(24)";
-            case "double":
-            case "Double":
-                return "FLOAT(53)";
-            case "boolean":
-            case "Boolean":
-                return "BOOLEAN";
-            case "Date":
-                return "DATE";
-            default:
-                return "varchar(255)";
-        }
     }
 }
